7719813952d4f7bc447511deefb8dc165b889dbe
javiblancotorres <javiblancotorres@gmail.com>
2014-11-05 18:30:09 +0100

    Tarea 01-Movimiento básico
    Movimiento horizontal del personaje.
    Añadido Circle Collider en los pies para que suba los escalones.
    
    PENDIENTE: Salto y animaciones.

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+	public float speed = 10f;
+	public Vector2 maxVelocity = new Vector2(2,3);
+	//Con esteo ultimo le estamos añadiendo una velocidad maxima al personaje.
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+				var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
+				var forceX = 0f;
+				var forceY = 0f;
+
+
+		if(Input.GetKey ("right")) {
+
+		if (absVelX < maxVelocity.x)
+			forceX = speed;
+		this.transform.localScale = new Vector3(1, 1, 1);
+	}
+	else if (Input.GetKey ("left"))
+	        {
+			if(absVelX < maxVelocity.x)
+			forceX = -speed;
+		this.transform.localScale = new Vector3 (-1, 1, 1);
+			}
+	rigidbody2D.AddForce (new Vector2 (forceX, forceY));
+	}
+}
