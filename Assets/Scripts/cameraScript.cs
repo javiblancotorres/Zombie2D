2166615dcb87cae14db8e9ed022cbcdffb8651a0
javiblancotorres <javiblancotorres@gmail.com>
2014-11-06 19:03:51 +0100

    Tarea 03 - movimiento de cámara
    Añadido script de movimiento de cámara.

---
+++ Assets/Scripts/cameraScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript: MonoBehaviour {
+
+	public Transform target; //Hacer publica la opcion de transformacion target, dice ser, que el objeto que ponga aqui sera seguido por la camara
+	public float damping = 1; //Hacer publica la opcion damping, lo que tarda la camara en seguir tu movimiento.
+	public float lookAheadFactor = 3; //
+	public float lookAheadReturnSpeed = 0.5f;
+	public float lookAheadMoveThreshold = 0.1f;
+	public float yPosRestiction = 1;
+
+
+
+	float offsetZ; 
+	Vector3 lastTargetPosition;
+	Vector3 currentVelocity;
+	Vector3 lookAheadPos;
+
+
+
+	// Use this for initialization
+	void Start () {
+		lastTargetPosition = target.position;
+		offsetZ = (transform.position - target.position).z;
+		transform.parent = null;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// only update lookahead pos if accelerating or changed direction
+		float xMoveDelta = (target.position - lastTargetPosition).x;
+		
+		bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+		
+		if (updateLookAheadTarget) {
+			lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+		} else {
+			lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);	
+		}
+		
+		Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;
+		Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+		
+		newPos = new Vector3(newPos.x, Mathf.Clamp(newPos.y,yPosRestiction,Mathf.Infinity), newPos.z);
+		transform.position = newPos;
+		
+		lastTargetPosition = target.position;	
+	
+	}
+}
