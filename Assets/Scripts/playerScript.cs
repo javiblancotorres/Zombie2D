e60388470472a9a4c24999fd2ff97a5ca8f527f8
javiblancotorres <javiblancotorres@gmail.com>
2014-11-10 18:45:08 +0100

    Tarea 04 - Animaciones.
    Metidas animaciones en animator y en Script

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -5,13 +5,15 @@ public class playerScript : MonoBehaviour {
 
 	public float speed = 10f;
 	public Vector2 maxVelocity = new Vector2(2,3);
-	//Con esteo ultimo le estamos añadiendo una velocidad maxima al personaje.
-
+	//Con esto ultimo le estamos añadiendo una velocidad maxima al personaje.
+	private Animator animator;
 
 
 	// Use this for initialization
 	void Start () {
-	
+
+		animator = GetComponent<Animator> ();
+	 
 	}
 	
 	// Update is called once per frame
@@ -24,16 +26,38 @@ public class playerScript : MonoBehaviour {
 
 		if(Input.GetKey ("right")) {
 
+			//esto lo que hace es frenar cuando voy a izquierda y pulso derecha
+			if (rigidbody2D.velocity.x < 0){
+				rigidbody2D.velocity = new Vector2(0,rigidbody2D.velocity.y);
+			}
 		if (absVelX < maxVelocity.x)
 			forceX = speed;
+			//esto de abajo pone al script mirando a la derecha
 		this.transform.localScale = new Vector3(1, 1, 1);
 	}
 	else if (Input.GetKey ("left"))
 	        {
+			//esto lo que hace es frenar cuando voy a derecha y pulso izquierda
+			if(rigidbody2D.velocity.x > 0){
+				rigidbody2D.velocity = new Vector2(0,rigidbody2D.velocity.y);
+			}
+
 			if(absVelX < maxVelocity.x)
 			forceX = -speed;
+
+			//esto de abajo pone al script mirando a la izquierda.
 		this.transform.localScale = new Vector3 (-1, 1, 1);
 			}
 	rigidbody2D.AddForce (new Vector2 (forceX, forceY));
+	
+		if (absVelX > 0)
+						animator.SetFloat ("Velocity", absVelX);
+
+		if (Input.GetKey ("c")) {
+						animator.SetBool ("Fire", true);
+				} else {
+			animator.SetBool ("Fire", false);
+				}
+
 	}
 }
