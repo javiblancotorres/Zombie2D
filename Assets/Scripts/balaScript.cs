cf75b108ca337bea24a20973b628dcd4d86bd166
javiblancotorres <javiblancotorres@gmail.com>
2014-11-12 18:48:06 +0100

    Tarea 06 - Partícula Bala
    Añadir script de bala y sistemas de partículas

--- Assets/Scripts/balaScript.cs
+++ Assets/Scripts/balaScript.cs
@@ -5,8 +5,30 @@ public class balaScript : MonoBehaviour {
 
 
 	public Vector2 velocity = new Vector2 (5, 0);
+	public GameObject particulas;
+
 
 	void Start () {
-		rigidbody2D.velocity = velocity * transform.localScale.x;
+		//rigidbody2D.velocity = velocity * transform.localScale.x;
+		rigidbody2D.AddForce (velocity * transform.localScale.x, ForceMode2D.Impulse);
+		//En vez de velocity, al hacerlo de la manera AddForce y el ForceMode2D.Impulse la bala recibira un impulso
 	}
+
+
+	void OnCollisionEnter2D(Collision2D target){
+		//Destroy (gameObject);
+		explotaBala ();
+	
+	
+	}
+
+	void explotaBala(){
+		var clone = Instantiate (particulas, transform.position, Quaternion.identity)
+			as GameObject;
+		Destroy (clone, 1);
+		Destroy (gameObject);
+	
+	}
+
+
 }
--- Assets/Scripts/disparoScript.cs
+++ Assets/Scripts/disparoScript.cs
@@ -8,6 +8,8 @@ public class disparoScript : MonoBehaviour {
 
 
 	public void Fire (){
+	    
+		//Si el jugador tiene bala distinta a  null (ninguna) generara un disparo desde el puntoDisparo y con rotacion estandar, y si el jugador no tiene bala, no pasara nada.
 		if (bala != null) {
 			var clone = Instantiate (bala, puntoDisparo.position,
 			                         Quaternion.identity) as GameObject;
