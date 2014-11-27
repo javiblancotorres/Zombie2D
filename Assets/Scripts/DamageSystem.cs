ec21bd659e8d5e3bc446759bfefac46cfd206204
javiblancotorres <javiblancotorres@gmail.com>
2014-11-27 19:14:46 +0100

    Sistema vida y barras

--- Assets/Scripts/DamageSystem.cs
+++ Assets/Scripts/DamageSystem.cs
@@ -9,18 +9,23 @@ public class DamageSystem : MonoBehaviour {
 	public RectTransform livebar;
 	
 	private float rectLive; 
-	
+	private int maxLive;
 	// Use this for initialization
 	void Start () {
 		if(livebar!=null){
 			rectLive = livebar.rect.width / live;
 		}
+		maxLive = live;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(live <=0){
 			muerte();
+		}
+		//Con esto lo que decimos es que si tienes la maxLive, cuando te den, no suba, se igual a maxLive
+		if(live > maxLive){
+			live = maxLive;
 		}
 	}
 	
